32bc930e714017d8a1258bb3cadb76293dd33c2f
Brooke Tweddell <brookealexandramedia.com>
2019-08-21 15:57:04 -0400

    Update nav.php

---
+++ global/sections/nav.php
@@ -0,0 +1,45 @@
+<!-- Header-->
+      <header class="page-header">
+        <!-- RD Navbar-->
+        <div class="rd-navbar-wrap">
+          <nav class="rd-navbar rd-navbar-1" data-layout="rd-navbar-fixed" data-sm-layout="rd-navbar-fixed" data-md-layout="rd-navbar-fixed" data-md-device-layout="rd-navbar-fixed" data-lg-layout="rd-navbar-fullwidth" data-lg-device-layout="rd-navbar-fixed" data-xl-layout="rd-navbar-static" data-xl-device-layout="rd-navbar-static" data-xxl-layout="rd-navbar-static" data-xxl-device-layout="rd-navbar-static" data-lg-stick-up-offset="150px" data-xl-stick-up-offset="150px" data-xxl-stick-up-offset="150px" data-lg-stick-up="true" data-xl-stick-up="true" data-xxl-stick-up="true" data-stick-up-clone="true">
+            <div class="rd-navbar-inner">
+              <!-- RD Navbar Panel-->
+              <div class="rd-navbar-panel">
+                <!-- RD Navbar Toggle-->
+                <button class="rd-navbar-toggle" data-rd-navbar-toggle=".rd-navbar"><span></span></button>
+                <!-- RD Navbar Brand-->
+                <div class="rd-navbar-brand"><a href="index.html"><img class="logo-inverse" src="images/logo-inverse-36x74.png" width="36" height="74" alt=""><img class="logo-default" src="images/logo-default-36x45.png" width="36" height="74" alt=""></a></div>
+              </div>
+              <div class="rd-navbar-nav-wrap">
+                <ul class="rd-navbar-nav">
+                  <li class="rd-nav-item active"><a class="rd-nav-link" href="index.html">Home Page</a>
+                  </li>
+                  <li class="rd-nav-item"><a class="rd-nav-link" href="about-us.html">About Us</a>
+                    <ul class="rd-menu rd-navbar-dropdown">
+                      <li class="rd-dropdown-item"><a class="rd-dropdown-link" href="#">history</a>
+                      </li>
+                      <li class="rd-dropdown-item"><a class="rd-dropdown-link" href="#">Staff</a>
+                      </li>
+                      <li class="rd-dropdown-item"><a class="rd-dropdown-link" href="#">News</a>
+                        <ul class="rd-menu rd-navbar-dropdown">
+                          <li class="rd-dropdown-item"><a class="rd-dropdown-link" href="#">Fresh</a>
+                          </li>
+                          <li class="rd-dropdown-item"><a class="rd-dropdown-link" href="#">Archive</a>
+                          </li>
+                        </ul>
+                      </li>
+                    </ul>
+                  </li>
+                  <li class="rd-nav-item"><a class="rd-nav-link" href="gallery.html">Gallery</a>
+                  </li>
+                  <li class="rd-nav-item"><a class="rd-nav-link" href="services.html">Services</a>
+                  </li>
+                  <li class="rd-nav-item"><a class="rd-nav-link" href="contacts.html">Contacts</a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </nav>
+        </div>
+      </header>
