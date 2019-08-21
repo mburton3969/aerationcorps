c10cae6cdbdf68210829be96fe87905570de99b0
Brooke Tweddell <brookealexandramedia.com>
2019-08-21 14:49:20 -0400

    sectioned "global" in different pages and put content in those pages
    sectioned "global" in different pages and put content in those pages

--- index.php
+++ index.php
@@ -1,71 +1,12 @@
 <!DOCTYPE html>
 <html class="wide wow-animation" lang="en">
   <head>
-    <title>Home Page</title>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, height=device-height, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <link rel="icon" href="images/favicon.ico" type="image/x-icon">
-    <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Unica+One%7COpen+Sans:400,600,700,300%7CCabin:400,500,700,600">
-    <link rel="stylesheet" href="css/style.css">
-    <style>.ie-panel{display: none;background: #212121;padding: 10px 0;box-shadow: 3px 3px 5px 0 rgba(0,0,0,.3);clear: both;text-align:center;position: relative;z-index: 1;} html.ie-10 .ie-panel, html.lt-ie-10 .ie-panel {display: block;}</style>
+    
   </head>
   <body>
-    <div class="ie-panel"><a href="http://windows.microsoft.com/en-US/internet-explorer/"><img src="images/ie8-panel/warning_bar_0000_us.jpg" height="42" width="820" alt="You are using an outdated browser. For a faster, safer browsing experience, upgrade for free today."></a></div>
-    <div class="preloader">
-      <div class="preloader-body">
-        <div class="cssload-container">
-          <div class="cssload-speeding-wheel"></div>
-        </div>
-        <p>Loading...</p>
-      </div>
-    </div>
+
+<!-- *** Page Content Goes Here *** -->
     <div class="page">
-      <!-- Header-->
-      <header class="page-header">
-        <!-- RD Navbar-->
-        <div class="rd-navbar-wrap">
-          <nav class="rd-navbar rd-navbar-1" data-layout="rd-navbar-fixed" data-sm-layout="rd-navbar-fixed" data-md-layout="rd-navbar-fixed" data-md-device-layout="rd-navbar-fixed" data-lg-layout="rd-navbar-fullwidth" data-lg-device-layout="rd-navbar-fixed" data-xl-layout="rd-navbar-static" data-xl-device-layout="rd-navbar-static" data-xxl-layout="rd-navbar-static" data-xxl-device-layout="rd-navbar-static" data-lg-stick-up-offset="150px" data-xl-stick-up-offset="150px" data-xxl-stick-up-offset="150px" data-lg-stick-up="true" data-xl-stick-up="true" data-xxl-stick-up="true" data-stick-up-clone="true">
-            <div class="rd-navbar-inner">
-              <!-- RD Navbar Panel-->
-              <div class="rd-navbar-panel">
-                <!-- RD Navbar Toggle-->
-                <button class="rd-navbar-toggle" data-rd-navbar-toggle=".rd-navbar"><span></span></button>
-                <!-- RD Navbar Brand-->
-                <div class="rd-navbar-brand"><a href="index.html"><img class="logo-inverse" src="images/logo-inverse-36x74.png" width="36" height="74" alt=""><img class="logo-default" src="images/logo-default-36x45.png" width="36" height="74" alt=""></a></div>
-              </div>
-              <div class="rd-navbar-nav-wrap">
-                <ul class="rd-navbar-nav">
-                  <li class="rd-nav-item active"><a class="rd-nav-link" href="index.html">Home Page</a>
-                  </li>
-                  <li class="rd-nav-item"><a class="rd-nav-link" href="about-us.html">About Us</a>
-                    <ul class="rd-menu rd-navbar-dropdown">
-                      <li class="rd-dropdown-item"><a class="rd-dropdown-link" href="#">history</a>
-                      </li>
-                      <li class="rd-dropdown-item"><a class="rd-dropdown-link" href="#">Staff</a>
-                      </li>
-                      <li class="rd-dropdown-item"><a class="rd-dropdown-link" href="#">News</a>
-                        <ul class="rd-menu rd-navbar-dropdown">
-                          <li class="rd-dropdown-item"><a class="rd-dropdown-link" href="#">Fresh</a>
-                          </li>
-                          <li class="rd-dropdown-item"><a class="rd-dropdown-link" href="#">Archive</a>
-                          </li>
-                        </ul>
-                      </li>
-                    </ul>
-                  </li>
-                  <li class="rd-nav-item"><a class="rd-nav-link" href="gallery.html">Gallery</a>
-                  </li>
-                  <li class="rd-nav-item"><a class="rd-nav-link" href="services.html">Services</a>
-                  </li>
-                  <li class="rd-nav-item"><a class="rd-nav-link" href="contacts.html">Contacts</a>
-                  </li>
-                </ul>
-              </div>
-            </div>
-          </nav>
-        </div>
-      </header>
       <!-- Swiper-->
       <section class="swiper-container swiper-slider" data-loop="true" data-autoplay="5000" data-simulate-touch="false">
         <div class="swiper-wrapper">
@@ -282,31 +223,14 @@
           </ul>
         </div>
       </section>
-      <footer class="page-footer">
-        <div class="container">
-          <div class="row row-30">
-            <div class="col-md-5">
-              <div class="contact-info text-center text-md-left">
-                <address>28 Jackson Blvd Ste 1020, Chicago <br class="d-none d-xl-block">IL 60604-2340
-                </address>
-              </div>
-            </div>
-            <div class="col-md-3">
-              <ul class="inline-list">
-                <li><a class="fl-glypho-facebook2" href="#"></a></li>
-                <li><a class="fl-glypho-twitter" href="#"></a></li>
-                <li><a class="fl-glypho-google-plus" href="#"></a></li>
-                <li><a class="fl-glypho-instagram19" href="#"></a></li>
-              </ul>
-            </div>
-            <div class="col-md-4 text-center text-md-right">
-              <p class="rights copyright"><span>&copy;&nbsp;</span><span class="copyright-year"></span><span>&nbsp;</span><a href="privacy-policy.html">Privacy Policy</a><br/>Design&nbsp;by&nbsp;<a href="https://zemez.io/">Zemez</a></p>
-            </div>
-          </div>
-        </div>
-      </footer>
     </div>
+    <!-- *** Page Content Ends Here *** -->
+
+    <!-- Page Footer-->
+
+    <!-- Global Mailform Output-->
     <div class="snackbars" id="form-output-global"></div>
+    <!-- Javascript-->
     <script src="js/core.min.js"></script>
     <script src="js/script.js"></script>
   </body>
