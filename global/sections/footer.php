5a6f7641c418bcd0f0a15f3c7f89dfc6d706bce0
Brooke Tweddell <brookealexandramedia.com>
2019-08-21 15:57:53 -0400

    Update footer.php

---
+++ global/sections/footer.php
@@ -0,0 +1,23 @@
+      <footer class="page-footer">
+        <div class="container">
+          <div class="row row-30">
+            <div class="col-md-5">
+              <div class="contact-info text-center text-md-left">
+                <address>28 Jackson Blvd Ste 1020, Chicago <br class="d-none d-xl-block">IL 60604-2340
+                </address>
+              </div>
+            </div>
+            <div class="col-md-3">
+              <ul class="inline-list">
+                <li><a class="fl-glypho-facebook2" href="#"></a></li>
+                <li><a class="fl-glypho-twitter" href="#"></a></li>
+                <li><a class="fl-glypho-google-plus" href="#"></a></li>
+                <li><a class="fl-glypho-instagram19" href="#"></a></li>
+              </ul>
+            </div>
+            <div class="col-md-4 text-center text-md-right">
+              <p class="rights copyright"><span>&copy;&nbsp;</span><span class="copyright-year"></span><span>&nbsp;</span><a href="privacy-policy.html">Privacy Policy</a><br/>Design&nbsp;by&nbsp;<a href="https://zemez.io/">Zemez</a></p>
+            </div>
+          </div>
+        </div>
+      </footer>
