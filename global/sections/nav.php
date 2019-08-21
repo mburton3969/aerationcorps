6ae41f65273ec99132bbe108f457b41ceed5b983
Brooke Tweddell <brookealexandramedia.com>
2019-08-21 16:47:19 -0400

    fixed nav and created multiple pages
    fixed nav and created multiple pages

--- global/sections/nav.php
+++ global/sections/nav.php
@@ -1,8 +1,8 @@
-<!-- Header-->
-      <header class="page-header">
+      <!-- Header-->
+      <header class="page-header" id="header">
         <!-- RD Navbar-->
         <div class="rd-navbar-wrap">
-          <nav class="rd-navbar rd-navbar-1" data-layout="rd-navbar-fixed" data-sm-layout="rd-navbar-fixed" data-md-layout="rd-navbar-fixed" data-md-device-layout="rd-navbar-fixed" data-lg-layout="rd-navbar-fullwidth" data-lg-device-layout="rd-navbar-fixed" data-xl-layout="rd-navbar-static" data-xl-device-layout="rd-navbar-static" data-xxl-layout="rd-navbar-static" data-xxl-device-layout="rd-navbar-static" data-lg-stick-up-offset="150px" data-xl-stick-up-offset="150px" data-xxl-stick-up-offset="150px" data-lg-stick-up="true" data-xl-stick-up="true" data-xxl-stick-up="true" data-stick-up-clone="true">
+          <nav class="rd-navbar" data-layout="rd-navbar-fixed" data-sm-layout="rd-navbar-fixed" data-md-layout="rd-navbar-fixed" data-md-device-layout="rd-navbar-fixed" data-lg-layout="rd-navbar-fullwidth" data-lg-device-layout="rd-navbar-fixed" data-xl-layout="rd-navbar-static" data-xl-device-layout="rd-navbar-static" data-xxl-layout="rd-navbar-static" data-xxl-device-layout="rd-navbar-static" data-lg-stick-up-offset="150px" data-xl-stick-up-offset="150px" data-xxl-stick-up-offset="150px" data-lg-stick-up="true" data-xl-stick-up="true" data-xxl-stick-up="true" data-stick-up-clone="true">
             <div class="rd-navbar-inner">
               <!-- RD Navbar Panel-->
               <div class="rd-navbar-panel">
@@ -13,29 +13,19 @@
               </div>
               <div class="rd-navbar-nav-wrap">
                 <ul class="rd-navbar-nav">
-                  <li class="rd-nav-item active"><a class="rd-nav-link" href="index.html">Home Page</a>
+                  <li class="rd-nav-item"><a class="rd-nav-link" href="index.php">Home Page</a>
                   </li>
-                  <li class="rd-nav-item"><a class="rd-nav-link" href="about-us.html">About Us</a>
+                  <li class="rd-nav-item active"><a class="rd-nav-link" href="about-us.php">About Us</a>
                     <ul class="rd-menu rd-navbar-dropdown">
-                      <li class="rd-dropdown-item"><a class="rd-dropdown-link" href="#">history</a>
-                      </li>
-                      <li class="rd-dropdown-item"><a class="rd-dropdown-link" href="#">Staff</a>
-                      </li>
-                      <li class="rd-dropdown-item"><a class="rd-dropdown-link" href="#">News</a>
-                        <ul class="rd-menu rd-navbar-dropdown">
-                          <li class="rd-dropdown-item"><a class="rd-dropdown-link" href="#">Fresh</a>
-                          </li>
-                          <li class="rd-dropdown-item"><a class="rd-dropdown-link" href="#">Archive</a>
-                          </li>
-                        </ul>
+                      <li class="rd-dropdown-item"><a class="rd-dropdown-link" href="#">FAQ</a>
                       </li>
                     </ul>
                   </li>
-                  <li class="rd-nav-item"><a class="rd-nav-link" href="gallery.html">Gallery</a>
-                  </li>
                   <li class="rd-nav-item"><a class="rd-nav-link" href="services.html">Services</a>
                   </li>
-                  <li class="rd-nav-item"><a class="rd-nav-link" href="contacts.html">Contacts</a>
+                  <li class="rd-nav-item"><a class="rd-nav-link" href="gallery.html">Sign Up</a>
+                  </li>
+                  <li class="rd-nav-item"><a class="rd-nav-link" href="contacts.html">Contact Us</a>
                   </li>
                 </ul>
               </div>
