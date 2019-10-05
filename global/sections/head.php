5d391826fabab7f05156824cc88bfa82a78d7044
Michael Burton <michael@burtonsolution.com>
2019-10-04 19:28:47 -0500

    added cache buster to CSS file

--- global/sections/head.php
+++ global/sections/head.php
@@ -9,7 +9,7 @@
     <META NAME="Description" CONTENT="A Loudoun County based company with highly trained technicians who specialize in Aeration Service using our own commercial equipment.">	
     <link rel="icon" href="images/brand/favicon.PNG">
     <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Unica+One%7COpen+Sans:400,600,700,300%7CCabin:400,500,700,600">
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/style.css?cb=<?php echo uniqid(); ?>">
     <style>.ie-panel{display: none;background: #212121;padding: 10px 0;box-shadow: 3px 3px 5px 0 rgba(0,0,0,.3);clear: both;text-align:center;position: relative;z-index: 1;} html.ie-10 .ie-panel, html.lt-ie-10 .ie-panel {display: block;}</style>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
       
