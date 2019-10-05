00e2bc9e0b13c6a9364eb47a89dbb9dc5d7615ef
Michael Burton <michael@burtonsolution.com>
2019-10-04 19:22:00 -0500

    Changes to Website for conversions

--- index.php
+++ index.php
@@ -15,14 +15,14 @@
 <!-- *** Page Content Goes Here *** -->
 
  <!-- FALL SPECIAL-->
-      <section class="well-xl-var-1 lawn-turf-2">
-        <div class="container text-center">
+      <section class="well-xl-var-1 lawn-turf-2" style="background-size: 120% 100%;">
+        <div class="container text-center" style="margin-top: -85px;">
           <h1 style="color: #f3b143; text-shadow: 4px 4px #041103;">FALL SPECIAL</h1>
           <h2 class="text-bold text-shadow" style="color: #ffff; text-shadow: 2px 2px #041103;">$95 to aerate your ¼ acre lawn!</h2>
           <p>
 
           </p>
-          <a class="btn btn-md btn-secondary-var-2 offset-2" href="sign-up.php">Reserve Your Service Today!</a>
+          <a class="btn btn-md btn-secondary-var-2b offset-2" href="sign-up.php">Reserve Your Service Today!</a>
            <p>
 
           </p>
